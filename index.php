5f0738de5df5c1af6375303f57442ca9cbe97b78
Michael Gimbel <gimbel0893@gmail.com>
2013-01-09 14:03:03 -0500

    fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     $_SESSION['count'] += 1;
 ?>
 <br /><br /><br /><br /><br />
-host: <?=$_ENV['HOSTNAME']?>
+host: <?=shell_exec('curl -s http://169.254.169.254/latest/meta-data/public-hostname')?>
 <br />
 count: <?=$_SESSION['count']?>
 <br /><br /><br /><br /><br />
